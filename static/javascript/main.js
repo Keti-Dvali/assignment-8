f9b33252dc57e5ad4d2fae16830349329735368a
Keti-Dvali <104909714+Keti-Dvali@users.noreply.github.com>
2022-08-05 13:39:41 -0700

    finished

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,10 +1,9 @@
 let User = {
-    myName: "Keti";
-    myLastName: "Dvali";
-    myAge: 22;
-    email: "ketevan.dvali.2@iliauni.edu.ge";
+    myName: "Keti",
+    myLastName: "Dvali",
+    myAge: 22,
+    myEmail: "ketevan.dvali.2@iliauni.edu.ge",
+    municipality: "Kharagauli"
 }
 
-console.log(User.myName)
-
-// let myName = "Keti";
+console.log(User.myName, User.myLastName, User.myAge, User.myEmail, User.municipality)
